03e8713bfb99626a23588540571e501ff3f7a1cc
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-07-31 07:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (5.99%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,40 +1,38 @@
-class Solution {
+ class Solution {
 public:
-   
+    int offset = 102;
 
-    bool check(int index, int op_bra, string &s, vector<vector<int>> &dp) {
-
-        if(op_bra<0)
+    bool check(int index, int bal, string &s, vector<vector<int>> &dp) {
+        if(bal<0)
         return false;
         if (index == s.size()) {
-            return op_bra == 0;
+            return bal == 0;
         }
 
 
             
-        if (dp[index][op_bra] != -1)
-            return dp[index][op_bra];
+        if (dp[index][bal + offset] != -1)
+            return dp[index][bal + offset];
 
-            
         bool ans = false;
 
         if (s[index] == '(') {
-            ans = check(index + 1, op_bra + 1, s, dp);
+            ans = check(index + 1, bal + 1, s, dp);
         } else if (s[index] == ')') {
-            ans = check(index + 1, op_bra - 1, s, dp);
+            ans = check(index + 1, bal - 1, s, dp);
         } else {
-            bool a = check(index + 1,op_bra + 1, s, dp);
-            bool b = check(index + 1, op_bra - 1, s, dp);
-            bool c = check(index + 1, op_bra, s, dp);
+            bool a = check(index + 1, bal + 1, s, dp);
+            bool b = check(index + 1, bal - 1, s, dp);
+            bool c = check(index + 1, bal, s, dp);
             ans = a | b | c;
         }
 
-        return dp[index][op_bra] = ans;
+        return dp[index][bal + offset] = ans;
     }
 
     bool checkValidString(string s) {
         int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1 + offset, -1));
         return check(0, 0, s, dp);
     }
 };
