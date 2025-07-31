3375ce7f14c23306bb6b2db1085b19c580c4c8b3
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-07-31 08:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (47.63%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,38 +1,30 @@
- class Solution {
+class Solution {
 public:
-    int offset = 102;
-
-    bool check(int index, int bal, string &s, vector<vector<int>> &dp) {
-        if(bal<0)
-        return false;
-        if (index == s.size()) {
-            return bal == 0;
-        }
-
-
-            
-        if (dp[index][bal + offset] != -1)
-            return dp[index][bal + offset];
-
-        bool ans = false;
-
-        if (s[index] == '(') {
-            ans = check(index + 1, bal + 1, s, dp);
-        } else if (s[index] == ')') {
-            ans = check(index + 1, bal - 1, s, dp);
-        } else {
-            bool a = check(index + 1, bal + 1, s, dp);
-            bool b = check(index + 1, bal - 1, s, dp);
-            bool c = check(index + 1, bal, s, dp);
-            ans = a | b | c;
-        }
-
-        return dp[index][bal + offset] = ans;
-    }
-
     bool checkValidString(string s) {
-        int n = s.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1 + offset, -1));
-        return check(0, 0, s, dp);
+        int l=0;
+        int h=0;
+        for(char c:s)
+        {
+            if(c=='(')
+            {
+                l++;
+                h++;
+            }
+            else if(c==')')
+            {
+                l--;
+                h--;
+            }
+            else
+            {
+                l--;
+                h++;
+            }
+            if(h<0)
+        return false;
+         l=max(l,0);
+        }
+        
+         return l==0;
     }
 };
