771f6460f1cf6f285420dcc9edffae50bd079159
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-08-26 10:26:48 +0530

    Time: 7 ms (41.71%) | Memory: 23.8 MB (80.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end());
-        vector<vector<int>>ans;
-        int st1;
-          int st=intervals[0][0];
-          int end=intervals[0][1];
-        for(int i=1;i<intervals.size();i++)
-        {
-          
-             
-             if(end>=intervals[i][0])
-             {
-                end=max(end,intervals[i][1]);
-                
-             }
-             else
-             {
-                ans.push_back({st,end});
-                st=intervals[i][0];
-                end=intervals[i][1];
-             }
-
-
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        int st = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (end >= intervals[i][0]) {
+                end = max(end, intervals[i][1]);
+            } else {
+                ans.push_back({st, end});
+                st = intervals[i][0];
+                end = intervals[i][1];
+            }
         }
         ans.push_back({st, end});
         return ans;
+    }
+
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        auto it = lower_bound(intervals.begin(), intervals.end(), newInterval,
+                              [](const vector<int>& a, const vector<int>& b) {
+                                  return a[0] < b[0]; 
+                              });
+        
+       
+        intervals.insert(it, newInterval);
+
+      
+        return merge(intervals);
     }
 };
