2e9503b71dd17337b8e22accf258dbc4ec2b7f11
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-08-27 18:14:42 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (85.30%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int patches=0;
+        long long max_reachable_number=0;
+    int i=0;
+        while(max_reachable_number<n)
+        {
+            if(i<nums.size()&&nums[i]<=max_reachable_number+1 )
+            {
+                max_reachable_number+=nums[i];
+                i++;
+            }
+            else 
+            {
+                
+                  max_reachable_number+=(max_reachable_number+1);
+                  patches++;
+            }
+
+        }
+        return patches;
+    }
+};
