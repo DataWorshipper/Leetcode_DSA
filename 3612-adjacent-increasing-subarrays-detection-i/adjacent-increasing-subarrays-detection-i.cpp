4bde6e77370a8aa42186d83d47122d51ef5175eb
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-10-14 09:57:45 +0530

    Time: 8 ms (89.38%) | Memory: 41.4 MB (14.16%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> prefix(n, 1);
-
-      
-        for(int i = 1; i < n; i++){
-            if(nums[i] > nums[i-1])
-                prefix[i] = prefix[i-1] + 1;
-        }
-
-       
-        for(int l = 0; l + 2*k - 1 < n; l++){
-            int mid = l + k - 1;
-            int r = l + 2*k - 1;
-
         
-            if(prefix[mid] < k) continue;
+        vector<int>prefix(nums.size(),1);
+        int n=nums.size();
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                prefix[i]=prefix[i-1]+1;
 
-            
-            int lenRight = prefix[r] - prefix[mid+1] + 1; 
-            if(lenRight >= k) return true;
+            }
+            else
+            continue;
+        }
+      
+    
+int l=0;
+int r=0;
+        while(r<n)
+        {   
+
+                if(r-l+1==2*k)
+                {   
+                    int diff1=0;
+                    int diff2=0;
+                    int mid=(l+r)/2;
+                    if(l==0)
+                    {diff1=prefix[mid];
+                    diff2=prefix[r]-prefix[mid+1]+1;
+                    }
+                    else
+                {
+                    diff1=prefix[mid]-prefix[l]+1;
+                    diff2=prefix[r]-prefix[mid+1]+1;
+                   
+
+                }
+                 if(diff1>=k&&diff2>=k)
+                    return true;
+                 l++;
+                
+                }
+
+          
+                r++;
         }
 
         return false;
