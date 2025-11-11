245746a3d9066d7d634d13cd9a68613bf4ee463c
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-11-11 10:39:02 +0530

    Time: 69 ms (91.26%) | Memory: 184.3 MB (21.36%) - LeetSync

--- 3725-maximum-and-minimum-sums-of-at-most-size-k-subarrays/maximum-and-minimum-sums-of-at-most-size-k-subarrays.cpp
+++ 3725-maximum-and-minimum-sums-of-at-most-size-k-subarrays/maximum-and-minimum-sums-of-at-most-size-k-subarrays.cpp
@@ -2,11 +2,9 @@ typedef long long ll;
 
 class Solution {
 public:
-
     ll calcMax(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> pge(n), nge(n);
-
         {
             vector<int> st;
             for(int i=0;i<n;i++){
@@ -23,29 +21,26 @@ public:
                 st.push_back(i);
             }
         }
-
         ll ans = 0;
         for(int i=0;i<n;i++){
             ll L = i - pge[i] - 1;
             ll R = nge[i] - i - 1;
             ll limit = min(L, (ll)k-1);
+            ll x0 = k - (R );
 
-            ll x0 = k-1-R;
+            ll a = min(limit, x0 - 1);
+            ll sum1 = (a >= 0 ? (a + 1) * (R + 1) : 0);
 
-            ll a = min(limit, x0-1);
-            ll cnt1 = (a >= 0 ? a+1 : 0);
-            ll sum1 = cnt1 * R;
-
-            ll l = max(0LL, x0);
+            ll b = max(0LL, x0);
             ll sum2 = 0;
-            if(l <= limit){
-                ll cnt2 = limit - l + 1;
-                ll first = k-1-l;
-                ll last = k-1-limit;
-                sum2 = cnt2 * (first + last) / 2;
+            if(b <= limit){
+                ll t = limit - b + 1;
+                ll first = k - b;
+                ll last  = k - limit;
+                sum2 = t * (first + last) / 2;
             }
 
-            ll ways = (limit + 1) + sum1 + sum2;
+            ll ways = sum1 + sum2;
             ans += ways * nums[i];
         }
         return ans;
@@ -54,7 +49,6 @@ public:
     ll calcMin(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> pse(n), nse(n);
-
         {
             vector<int> st;
             for(int i=0;i<n;i++){
@@ -71,29 +65,26 @@ public:
                 st.push_back(i);
             }
         }
-
         ll ans = 0;
         for(int i=0;i<n;i++){
             ll L = i - pse[i] - 1;
             ll R = nse[i] - i - 1;
             ll limit = min(L, (ll)k-1);
+            ll x0 = k - (R );
 
-            ll x0 = k-1-R;
+            ll a = min(limit, x0 - 1);
+            ll sum1 = (a >= 0 ? (a + 1) * (R + 1) : 0);
 
-            ll a = min(limit, x0-1);
-            ll cnt1 = (a >= 0 ? a+1 : 0);
-            ll sum1 = cnt1 * R;
-
-            ll l = max(0LL, x0);
+            ll b = max(0LL, x0);
             ll sum2 = 0;
-            if(l <= limit){
-                ll cnt2 = limit - l + 1;
-                ll first = k-1-l;
-                ll last = k-1-limit;
-                sum2 = cnt2 * (first + last) / 2;
+            if(b <= limit){
+                ll t = limit - b + 1;
+                ll first = k - b;
+                ll last  = k - limit;
+                sum2 = t * (first + last) / 2;
             }
 
-            ll ways = (limit + 1) + sum1 + sum2;
+            ll ways = sum1 + sum2;
             ans += ways * nums[i];
         }
         return ans;
