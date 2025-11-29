d7716aaa1e96969cf9c6acac24e91a6877fb9c58
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2025-11-29 08:42:03 +0530

    Time: 200 ms (44.44%) | Memory: 152.4 MB (44.44%) - LeetSync

--- 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
+++ 3344-minimize-manhattan-distances/minimize-manhattan-distances.cpp
@@ -1,48 +1,85 @@
+typedef long long ll;
+
 class Solution {
-private:
-    int manhattan(vector<vector<int>>& points, int i, int j) {
-        return abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
-    }
-    
-    pair<int, int> maxManhattanDistance(vector<vector<int>>& points, int remove = -1) {
-        int n = points.size();
-        int maxSum = INT_MIN, minSum = INT_MAX, maxDiff = INT_MIN, minDiff = INT_MAX;
-        int maxSumIndex, minSumIndex, maxDiffIndex, minDiffIndex;
-        for (int i = 0; i < n; i++) {
-            if (i != remove) {
-                int sum = points[i][0] + points[i][1],
-                    diff = points[i][0] - points[i][1];
-                if (sum > maxSum) {
-                    maxSumIndex = i;
-                    maxSum = sum;
-                }
-                if (sum < minSum) {
-                    minSumIndex = i;
-                    minSum = sum;
-                }
-                if (diff > maxDiff) {
-                    maxDiffIndex = i;
-                    maxDiff = diff;
-                }
-                if (diff < minDiff) {
-                    minDiffIndex = i;
-                    minDiff = diff;
-                }
-            }
-        }
-        return max(maxSum - minSum, maxDiff - minDiff) == maxSum - minSum
-            ? make_pair(maxSumIndex, minSumIndex)
-            : make_pair(maxDiffIndex, minDiffIndex);
-    }
-    
 public:
-    int minimumDistance(vector<vector<int>>& points) {
-        auto [mi, mj] = maxManhattanDistance(points);
-        auto [mi1, mj1] = maxManhattanDistance(points, mi); // remove mi
-        auto [mi2, mj2] = maxManhattanDistance(points, mj); // remove mj
-        return min(
-            manhattan(points, mi1, mj1),
-            manhattan(points, mi2, mj2)
-        );
+
+    vector<ll> f1(const vector<vector<int>>& points)
+    {
+        vector<ll> a(points.size());
+        for (ll i = 0; i < (ll)points.size(); i++) {
+            ll x = points[i][0];
+            ll y = points[i][1];
+            a[i] = x + y;
+        }
+        return a;
+    }
+
+    vector<ll> f2(const vector<vector<int>>& points)
+    {
+        vector<ll> a(points.size());
+        for (ll i = 0; i < (ll)points.size(); i++) {
+            ll x = points[i][0];
+            ll y = points[i][1];
+            a[i] = x - y;
+        }
+        return a;
+    }
+
+   
+    ll ans(vector<ll> s, vector<ll> d, ll idx)
+    {
+        // erase
+        s.erase(s.begin() + idx);
+        d.erase(d.begin() + idx);
+
+        sort(s.begin(), s.end());
+        sort(d.begin(), d.end());
+
+        if (s.size() == 0) return 0;
+
+        ll a = s.back() - s.front();
+        ll b = d.back() - d.front();
+
+        return max(a, b);
+    }
+
+    int minimumDistance(vector<vector<int>>& points)
+    {
+        ll n = points.size();
+
+        ll maxsum = LLONG_MIN, minsum = LLONG_MAX;
+        ll maxdiff = LLONG_MIN, mindiff = LLONG_MAX;
+        ll maxsumidx = 0, minsumidx = 0, maxdiffidx = 0, mindiffidx = 0;
+
+       
+        for (ll i = 0; i < n; i++) {
+            ll x = points[i][0];
+            ll y = points[i][1];
+            ll sum = x + y;
+            ll diff = x - y;
+
+            if (sum > maxsum) maxsum = sum, maxsumidx = i;
+            if (sum < minsum) minsum = sum, minsumidx = i;
+
+            if (diff > maxdiff) maxdiff = diff, maxdiffidx = i;
+            if (diff < mindiff) mindiff = diff, mindiffidx = i;
+        }
+
+        vector<ll> s = f1(points);
+        vector<ll> d = f2(points);
+
+     
+        ll a1 = ans(s, d, maxsumidx);
+        ll a2 = ans(s, d, minsumidx);
+        ll a3 = ans(s, d, maxdiffidx);
+        ll a4 = ans(s, d, mindiffidx);
+
+       
+        sort(s.begin(), s.end());
+        sort(d.begin(), d.end());
+        ll a5 = max(s.back() - s.front(), d.back() - d.front());
+
+        ll answer = min({a1, a2, a3, a4, a5});
+        return (int)answer;
     }
 };
