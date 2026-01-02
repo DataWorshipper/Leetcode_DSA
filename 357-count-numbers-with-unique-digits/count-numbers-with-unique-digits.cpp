ebf917cbd30f06de698b4ce60b7c646f14b2bbb1
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-01-02 08:54:48 +0530

    Time: 2 ms (9.79%) | Memory: 8 MB (7.49%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    int dp[10][2][2][1<<10];
+    public:
+int f(int index,int tight,int valid,int mask,string &s)
+{
+    if(index==s.size())
+    {
+        if(valid)
+        return 1;
+        else
+        return 0;
+    }
+
+    if(dp[index][tight][valid][mask]!=-1)
+    return dp[index][tight][valid][mask];
+
+    int limit=(tight==1)?s[index]-'0':9;
+    int ans=0;
+    for(int i=0;i<=limit;i++)
+    {
+        int newtight=tight&(i==s[index]-'0');
+
+       if(valid==0&&i==0)
+       {    
+        ans+=f(index+1,newtight,0,mask,s);
+       }
+       else if(valid==0 &&i>0)
+       {
+        ans+=f(index+1,newtight,1,mask^(1<<i),s);
+       }
+       else 
+       {
+        if(!(mask&(1<<i)))
+        {
+            ans+=f(index+1,newtight,1,mask^(1<<i),s);
+        }
+       }
+
+    
+    }
+       return dp[index][tight][valid][mask]=ans;
+}
+public:
+    int countNumbersWithUniqueDigits(int n) {
+      int x = 1;
+for(int i=0;i<n;i++) x *= 10;
+x--; 
+string s = to_string(x);
+
+        memset(dp,-1,sizeof(dp));
+        return f(0,1,0,0,s)+1;
+    }
+};
