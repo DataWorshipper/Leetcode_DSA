b89f5c7f80445651245fb12deeb051f62ca576d1
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-01-13 09:37:10 +0530

    Time: 100 ms (61.56%) | Memory: 198.7 MB (52.02%) - LeetSync

---
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -0,0 +1,55 @@
+typedef long long ll;
+class Solution {
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        double low=LLONG_MAX;
+        double high=LLONG_MIN;
+        for(ll i=0;i<squares.size();i++)
+        {
+            double y=(double)squares[i][1];
+            low=min(low,y);
+            high=max(high,y+(double)squares[i][2]);
+        }
+    double ans=0;
+    double eps=1e-6;
+        while(high-low>=eps)
+        {
+            double mid=(low)+(high-low)/2.0;
+            double area_below=0.0;
+            double area_above=0.0;
+            for(ll i=0;i<squares.size();i++)
+            {
+                 double y=(double)squares[i][1];
+                 double len=(double)squares[i][2];
+                 if(y>=mid)
+                 {
+                    area_above+=(len*len);
+                 }
+                 else if(y+len<=mid)
+                 {
+                    area_below+=(len*len);
+                 }
+                 else if(mid>=y &&mid<=y+len)
+                 {
+                    area_below+=(len*(abs(mid-y)));
+                    area_above+=(len*(y+len-mid));
+                 }
+            }
+
+            if(area_above<=area_below)
+            {   
+                ans=mid;
+                 high=mid; 
+
+            }
+            else
+            {
+                low=mid;
+            }
+        }
+
+        return ans;
+
+
+    }
+};
