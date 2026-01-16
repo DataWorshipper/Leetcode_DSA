a0e77a986cc4c22b4830640ad9063fef9421556c
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-01-16 11:18:44 +0530

    Time: 9 ms (46.57%) | Memory: 25.1 MB (9.39%) - LeetSync

--- 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
+++ 2340-total-appeal-of-a-string/total-appeal-of-a-string.cpp
@@ -1,101 +1,39 @@
-typedef long long ll;
-#define v vector
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-
 class Solution {
-    ll findRange(ll i, const v<v<ll>>& range) {
-        ll l = 0, r = (ll)range.size() - 1;
-        while (l <= r) {
-            ll mid = (l + r) / 2;
-            if (range[mid][0] <= i && i <= range[mid][1]) return mid;
-            else if (i < range[mid][0]) r = mid - 1;
-            else l = mid + 1;
-        }
-        return -1;
-    }
-
 public:
     long long appealSum(string s) {
-        ll n = s.size();
-
+        int n = s.size();
+        vector<vector<int>> pos(26);
+        
       
-        v<ll> prefix(n,0);
-        v<int> seen(26,0);
-        ll distinct = 0;
-        for(ll i=0;i<n;i++){
-            if(!seen[s[i]-'a']){
-                seen[s[i]-'a']=1;
-                distinct++;
-            }
-            prefix[i]=distinct;
+        for(int i = 0; i < n; i++){
+            pos[s[i] - 'a'].push_back(i);
         }
+
+        long long totalSub = 1LL * n * (n + 1) / 2;
+        long long ans = 0;
+
+        for(int c = 0; c < 26; c++){
+            if(pos[c].empty()) continue;
+
+            long long avoid = 0;
 
         
-        v<v<ll>> range;
-        ll f=0,l=0,x=prefix[0];
-        for(ll i=1;i<n;i++){
-            if(prefix[i]==x){
-                l++;
-            }else{
-                range.push_back({f,l});
-                f=i;
-                l=i;
-                x=prefix[i];
+            int first = pos[c][0];
+            avoid += 1LL * first * (first + 1) / 2;
+
+            for(int i = 1; i < pos[c].size(); i++){
+                int gap = pos[c][i] - pos[c][i-1] - 1;
+                avoid += 1LL * gap * (gap + 1) / 2;
             }
+
+            int last = pos[c].back();
+            int tail = n - 1 - last;
+            avoid += 1LL * tail * (tail + 1) / 2;
+
+            long long contain = totalSub - avoid;
+            ans += contain;
         }
-        range.push_back({f,l});
 
-    
-        v<ll> diff(range.size());
-        for(ll i=0;i<range.size();i++)
-            diff[i]=range[i][1]-range[i][0]+1;
-
-       
-        v<ll> prefix2(diff.size());
-        prefix2[0]=diff[0];
-        for(ll i=1;i<diff.size();i++)
-            prefix2[i]=prefix2[i-1]+diff[i];
-
-       
-        v<ll> pre(range.size(),0);
-        for(ll i=1;i<range.size();i++){
-            ll ans=0,freq=i+1;
-            for(ll j=0;j<i;j++){
-                ans += diff[j]*freq;
-                freq--;
-            }
-            pre[i]=ans;
-        }
-vector<ll> last(26, -1);
-ll curAdd = 0;  
-
-  ll appeal = 0;
-ll curRange = 0;
-ll start = range[0][0], end = range[0][1];
-
-for(ll i=0;i<n;i++){
-  
-    int ch = s[i] - 'a';
-    if(last[ch] == -1){
-        curAdd += (i + 1);
-    } else {
-        curAdd += (i - last[ch]);
-    }
-    last[ch] = i;
-
-    ll r = findRange(i,range);
-    ll before = appeal;
-
-    ll add = curAdd;  
-
-    appeal += add;}
-
-
-
-
-
-        return appeal;
+        return ans;
     }
 };
