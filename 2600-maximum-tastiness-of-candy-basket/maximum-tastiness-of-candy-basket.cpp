169f64c22e263d9d7bb41457352b99fe67d3ce6f
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-01-22 14:45:28 +0530

    Time: 46 ms (69.89%) | Memory: 52.3 MB (58.92%) - LeetSync

--- 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.cpp
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& price, int diff, int k) {
-        int last = price[0];
-        int count = 1;
-
-        for (int i = 1; i < price.size(); i++) {
-            if (price[i] - last >= diff) {
-                count++;
-                last = price[i];
-                if (count >= k) return true;
-            }
+bool check(int d,vector<int>& price, int k)
+{
+    int cnt=1;
+    int lastbuy=price[0];
+    for(int i=1;i<price.size();i++)
+    {
+        if(price[i]-lastbuy>=d)
+        {
+            cnt++;
+            lastbuy=price[i];
         }
-        return false;
     }
-
+    return (cnt>=k);
+}
     int maximumTastiness(vector<int>& price, int k) {
-        sort(price.begin(), price.end());
-        int low = 0; 
-        int high = price.back() - price.front();
-        int ans = 0;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (isPossible(price, mid, k)) {
-                ans = mid;      
-                low = mid + 1;
-            } else {
-                high = mid - 1; 
+        
+        int low=0;
+        sort(price.begin(),price.end());
+        int high=price.back()-price[0];
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(check(mid,price,k)==true)
+            {
+                ans=mid;
+                low=mid+1;
             }
+            else
+            high=mid-1;
         }
         return ans;
     }
