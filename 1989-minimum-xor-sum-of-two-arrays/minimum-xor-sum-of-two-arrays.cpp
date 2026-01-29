ac2c1fedcceb56b3a5f95eed744d2765852f2779
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-01-29 08:04:20 +0530

    Time: 11 ms (70.16%) | Memory: 13.3 MB (34.10%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
@@ -1,30 +1,32 @@
 typedef long long ll;
+
 class Solution {
 public:
-ll dp[15][1<<14];
-ll f(int index,vector<int>& nums1, vector<int>& nums2,int mask)
-{
-    if(index==nums1.size())
-    {
-        return 0;
-    }
-    if(dp[index][mask]!=-1)
-    return  dp[index][mask];
-    ll ans=LLONG_MAX;
-    for(int i=0;i<nums2.size();i++)
-    {
-        if(!(mask&(1<<i)))
-        {
-           ans=min((ll)ans,(nums1[index]^nums2[i])+f(index+1,nums1,nums2,mask|(1<<i)));
-        }
-    }
-    return dp[index][mask]=ans;
-}
-    int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+    ll dp[15][1 << 14];
 
-            int mask=0;
-            int index=0;
-            memset(dp,-1,sizeof(dp));
-            return (int)f(0,nums1,nums2,0);
+    ll f(int index, vector<int>& nums1, vector<int>& nums2, int mask) {
+        if (index == nums2.size()) {
+            return 0;
+        }
+
+        if (dp[index][mask] != -1)
+            return dp[index][mask];
+
+        ll ans = LLONG_MAX;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            if (!(mask & (1 << i))) { 
+                ans = min(ans,
+                          (ll)(nums1[i] ^ nums2[index]) +
+                          f(index + 1, nums1, nums2, mask | (1 << i)));
+            }
+        }
+
+        return dp[index][mask] = ans;
+    }
+
+    int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+        memset(dp, -1, sizeof(dp));
+        return (int)f(0, nums1, nums2, 0);
     }
 };
