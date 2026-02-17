c054082f8a9744cd01f003d97602c8eae8eae829
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-02-17 12:02:00 +0530

    Time: 5 ms (55.73%) | Memory: 9.6 MB (44.59%) - LeetSync

--- 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
+++ 1403-palindrome-partitioning-iii/palindrome-partitioning-iii.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-int getcost(int l,int r,string &s)
-{   
-    int cnt=0;
-    while(l<r)
-    {
-        if(s[l]!=s[r])
-        cnt++;
-        
-        l++;
-        r--;
-
+    int getcost(int l, int r, string &s) {
+        int cnt = 0;
+        while (l < r) {
+            if (s[l] != s[r]) cnt++;
+            l++;
+            r--;
+        }
+        return cnt;
     }
-    return cnt;
-}
+
     int palindromePartition(string s, int k1) {
-         int n = s.size();
+        int n = s.size();
         const int INF = 1e9;
 
-        vector<vector<int>> dp(n, vector<int>(k1+1, INF));
-      for(int i=0;i<n;i++)
-      {
-        dp[i][1]=getcost(0,i,s);
-      }
+   
+        vector<vector<int>> cost(n, vector<int>(n, 0));
+        for (int r = 0; r < n; r++) {
+            for (int l = 0; l <= r; l++) {
+                cost[l][r] = getcost(l, r, s);
+            }
+        }
 
-        for(int k=2;k<=k1;k++)
-        {
-            for(int i=0;i<s.size();i++)
-            {
-                for(int j=0;j<i;j++)
-                {
-                    dp[i][k]=min(dp[i][k],dp[j][k-1]+getcost(j+1,i,s));
+       
+        vector<vector<int>> dp(n, vector<int>(k1+1, INF));
+
+      
+        for (int i = 0; i < n; i++) {
+            dp[i][1] = cost[0][i];
+        }
+
+        for (int k = 2; k <= k1; k++) {
+            for (int i = 0; i < n; i++) {
+                if (k > i + 1) continue;
+
+                for (int j = 0; j < i; j++) {
+                    if (dp[j][k-1] == INF) continue;
+                    dp[i][k] = min(dp[i][k],
+                                   dp[j][k-1] + cost[j+1][i]);
                 }
             }
         }
-        return dp[s.size()-1][k1];
 
+        return dp[n-1][k1];
     }
 };
