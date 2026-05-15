0fb07ac9296abad373fc685a5195f6ee56b05e9d
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-05-15 10:12:02 +0530

    Time: 1495 ms (37.50%) | Memory: 25.4 MB (75.00%) - LeetSync

---
+++ 1680-count-all-possible-routes/count-all-possible-routes.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+
+        n=len(locations)
+        dp=[[-1]*(fuel+1) for _ in range(n)]
+        MOD=10**9+7
+        def f(i:int,fuel_left:int,start:int,finish:int,locations:List[int])->int:
+            ans = 1 if i == finish else 0
+            
+            if dp[i][fuel_left]!=-1:
+                return dp[i][fuel_left]
+            
+            for index,city in enumerate(locations):
+                f2=fuel_left-abs(locations[i]-city)
+                if locations[i]!=city and f2>=0:
+                    ans=(ans+f(index,fuel_left-abs(locations[i]-city),start,finish,locations))%MOD
+                
+            dp[i][fuel_left]=ans%MOD
+            return ans%MOD
+        
+        return  f(start,fuel,start,finish,locations)
+      
+        
+    
+
+
+            
+
+
+
+        
