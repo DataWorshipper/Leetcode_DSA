1a67e2c57a761a1b52b48ab3724a351f866cc43d
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-06-08 19:49:24 +0530

    Time: 677 ms (7.78%) | Memory: 36.2 MB (90.56%) - LeetSync

--- 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
-int dp[1001][1001];
-int rec(int i,int x,vector<int>& nums, int k,  map<int,vector<int>>&mpp)
-{
-    if(dp[i][x]!=-1)
+    int dp[1001][1001];
+
+    int rec(int x,
+            int i,
+            vector<int>& nums,
+            int k,
+            map<int, vector<int>>& mpp)
     {
-        return dp[i][x];
-    }
-    int ans=2;
-    auto it=lower_bound(mpp[nums[i]%k].begin(),mpp[nums[i]%k].end(),x);
-   
-    if(it!=mpp[nums[i]%k].begin())
-    {       
-        it--;
-          int y=*it;
-        
-        ans=max(ans,1+rec(x,y,nums,k,mpp));
-    }
-    return dp[i][x]=ans;
+        int &ans = dp[x][i];
+        if(ans != -1) return ans;
 
+        ans = 2;
 
-}
-    int maximumLength(vector<int>& nums, int k) {
-        map<int,vector<int>>mpp;
-        for(int i=0;i<nums.size();i++)
+        auto &v = mpp[nums[x] % k];
+
+        auto it = lower_bound(v.begin(), v.end(), i + 1);
+
+        if(it != v.end())
         {
-            mpp[nums[i]%k].push_back(i);
-        }
-    memset(dp,-1,sizeof(dp));
-    int ans=INT_MIN;
-            for(int i=0;i<nums.size();i++)
-            {
-                for(int j=0;j<i;j++)
-                {
-                    ans=max(ans,rec(i,j,nums,k,mpp));
-                }
-            }
-            return ans;
+            int y = *it;
 
+            ans = max(ans,
+                      1 + rec(i, y, nums, k, mpp));
+
+            ++it;
+        }
+
+        return ans;
+    }
+
+    int maximumLength(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        map<int, vector<int>> mpp;
+
+        for(int i = 0; i < n; i++)
+            mpp[nums[i] % k].push_back(i);
+
+        memset(dp, -1, sizeof(dp));
+
+        int ans = 1;
+
+        for(int x = 0; x < n; x++)
+        {
+            for(int i = x + 1; i < n; i++)
+            {
+                ans = max(ans,
+                          rec(x, i, nums, k, mpp));
+            }
+        }
+
+        return ans;
     }
 };
