4aec935f4579d3fb3645705364479d2ec684bee3
Abhiraj Mandal <152878306+DataWorshipper@users.noreply.github.com>
2026-09-22 11:58:10 +0530

    Time: 944 ms (92.44%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-
-with CTE as(
-select name,bonus from Employee left join Bonus on Employee.empId=Bonus.empId )
-
-select name,bonus from CTE where bonus<1000 or bonus is null
+select e1.name,b1.bonus from Employee as e1 left join
+Bonus as b1 on
+e1.empId=b1.empId
+where b1.bonus<1000 or b1.bonus is null
